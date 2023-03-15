96d15832004ebc5a45a73cc4cc099f054704e949
bbb169 <827088092@qq.com>
2023-03-15 14:56:12 +0800

    add notes for function maskUrl

--- src/utils/mask.ts
+++ src/utils/mask.ts
@@ -1,7 +1,7 @@
 export function maskUrl (box: HTMLElement, innerBox: HTMLElement) {
   const innerBoxSize = innerboxRect()
   const boxSize = box.getBoundingClientRect()
-  let url = `data:image/svg+xml;charset=utf8,<svg xmlns='http://www.w3.org/2000/svg' width='${boxSize.width}' height='${boxSize.height}'><mask id='mask'><rect x='0' y='0' width='${boxSize.width}' height='${boxSize.height}' fill='white'></rect>`
+  let url = `data:image/svg+xml;charset=utf8,<svg xmlns='http://www.w3.org/2000/svg' width='${boxSize.width}' height='${boxSize.height}'><mask id='mask'><rect x='0' y='0' width='${boxSize.width}' height='${boxSize.height}' fill='white'></rect>` //out box
 
   const x = (innerBoxSize.rect.x || innerBoxSize.rect.left) - (boxSize.x || boxSize.left)
   const y = (innerBoxSize.rect.y || innerBoxSize.rect.top) - (boxSize.y || boxSize.top)
@@ -11,7 +11,7 @@ export function maskUrl (box: HTMLElement, innerBox: HTMLElement) {
   url += `<rect x='${x}' y='${y}' width='${width}' height='${height}' ${
       innerBoxSize.cornerRadius ? `rx='${innerBoxSize.cornerRadius}' ry='${innerBoxSize.cornerRadius}'` : ''
     } fill='black'></rect>` //inner circle
-  url += `</mask><rect x='0' y='0' width='${boxSize.width}' height='${boxSize.height}' mask='url(%23mask)'></rect></svg>`
+  url += `</mask><rect x='0' y='0' width='${boxSize.width}' height='${boxSize.height}' mask='url(%23mask)'></rect></svg>` //out box
   box.style.webkitMask = 'url("' + url + '")'
   box.style.webkitMaskSize = '100% 100%'
 
